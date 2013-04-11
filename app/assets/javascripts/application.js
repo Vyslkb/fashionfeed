ba7dc833faf78463d6ca2f2700ca54c83002ea5e
Cameron Browning <camb416@gmail.com>
2013-04-10 21:25:10 -0400

    Lots of JS front-end work… overlay nav, etc

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -50,14 +50,120 @@ $container.imagesLoaded(function(){
 });
 
 }
-FASHIONFEED.showOverlay = function(hash_str){
-	$("#overlay").css('display','block');
-	$('#bigimage').attr("src",hash_str);
+FASHIONFEED.setBigImage  = function(data){
+ // $('#bigimage').attr("src",data.image_url);
+
+
+  var newImgID = 'img'+data.id;
+  $("#bigimage-container").append('<img id="'+newImgID+'" class="bigimage" src="'+data.image_url+'" onclick="FASHIONFEED.nextBigImage()" />')
+  $("#"+newImgID).animate({
+    opacity: 1.0
+  }, 200, function() {
+    // Animation complete.
+
+  });
 
 }
 
+FASHIONFEED.nextBigImage = function(sender){
+/*
+$('#img'+FASHIONFEED.element.id).animate({
+    visible: 'toggle'
+  }, 2000, function() {
+    // Animation complete.
+    $(this).remove();
+
+  });
+*/
+//$('#img'+FASHIONFEED.element.id).hide(2000);
+  $('#img'+FASHIONFEED.element.id).remove();
+
+  var nextID;
+  console
+  if(FASHIONFEED.element!=null){ // truthiness?
+    nextID = FASHIONFEED.element.id+1;
+  } else {
+    nextID = 1;
+  }
+  FASHIONFEED.updateOverlay(nextID);
+  console.log("next big image: "+nextID);
+}
+
+FASHIONFEED.updateOverlay = function(photo_id){
+  $.ajax({ type: 'GET',
+contentType: "application/json",
+url: "/photos/"+photo_id, 
+data:{ format: "json"}
+}).done(function(data) {
+FASHIONFEED.element = data;
+FASHIONFEED.setBigImage(data);
+
+})
+    .fail(function() { 
+
+      //alert("error"); 
+
+    })
+    .always(function() { 
+
+      //alert("complete"); 
+
+    });
+}
+
+FASHIONFEED.showOverlay = function(photo_id){
+
+FASHIONFEED.updateOverlay(photo_id);
+
+	$("#overlay").css('display','block');
+  $("#overlay").css('opacity',0);
+	
+
+
+  $('#overlay').animate({
+    opacity: 1.0
+  }, 500, function() {
+    // Animation complete.
+
+  });
+   $('.bigimage').animate({
+    opacity: 1.0
+  }, 500, function() {
+    // Animation complete.
+
+  });
+
+
+  
+
+
+
+
+}
+
+function onSuccess(){
+//alert("success");
+}
+function onError(){
+//alert("error");
+}
+
 FASHIONFEED.hideOverlay = function(){
-	$("#overlay").css('display','none');
+	//$("#overlay").css('display','none');
 	location.hash = "";
+
+  
+  $("#overlay").css('opacity',1.0);
+
+
+
+  $('#overlay').animate({
+    opacity: 0
+  }, 200, function() {
+    // Animation complete.
+    $("#overlay").css('display','none');
+    $("img.bigimage").remove();
+
+  });
 
 }
